6371c01c1512611a0c6d3ab74de5067e4f5f872c
duo530 <cavaluslg@gmail.com>
2025-03-25 00:13:24 -0300

    Update doomvideo.js

--- doomvideo.js
+++ doomvideo.js
@@ -1,9 +1,11 @@
-document.querySelectorAll('video').forEach(video => {
-    let iframe = document.createElement('iframe');
-    iframe.src = "https://dos.zone/doom-dec-1993/";
-    iframe.width = video.clientWidth;
-    iframe.height = video.clientHeight;
-    iframe.style.border = "none";
-    
-    video.parentNode.replaceChild(iframe, video);
-});
+let iframe = document.createElement('iframe');
+iframe.src = "https://dos.zone/doom-dec-1993/";
+iframe.width = "800";  // Largura do iframe (ajuste conforme necessário)
+iframe.height = "600"; // Altura do iframe
+iframe.style.position = "fixed";
+iframe.style.top = "10px";
+iframe.style.left = "10px";
+iframe.style.zIndex = "9999";
+iframe.style.border = "none";
+
+document.body.appendChild(iframe);
